237cf9abca5d80a5918f4eafd9b516d708978ac3
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-08-04 09:52:11 -0400

    Update adminController.js
    The team is on board with deleting create students functionality, so I deleted the function to create a new student, and the function to delete students.

--- gg-pac-team3-back/src/controllers/adminController.js
+++ gg-pac-team3-back/src/controllers/adminController.js
@@ -24,68 +24,6 @@ const verifyAdmin = async (req, res, next) => {
   }
 };
 
-// Function to create a new user
-const createUser = async (req, res) => {
-  const { name, email, password, role } = req.body;
-  const adminUid = req.adminUid;
-  // console.log('Creating user with admin UID:', adminUid);
-
-  try {
-    const userRecord = await admin.auth().createUser({
-      displayName: name,
-      email,
-      password,
-    });
-    // console.log('User created:', userRecord);
-
-    if (role === 'admin') {
-      await admin.auth().setCustomUserClaims(userRecord.uid, { admin: true });
-      // console.log('Admin claim set for new user:', userRecord.uid);
-    }
-
-    await db.collection('users').doc(userRecord.uid).set({
-      name,
-      email,
-      IsAdmin: role === 'admin',
-      createdBy: adminUid,
-      createdAt: admin.firestore.FieldValue.serverTimestamp(),
-    });
-    // console.log('New user details saved in Firestore:', userRecord.uid);
-
-    res.status(201).json(userRecord);
-  } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
-// Function to delete a user created by the admin
-const deleteUser = async (req, res) => {
-  const { uid } = req.params;
-  const adminUid = req.adminUid;
-  // console.log('Deleting user with admin UID:', adminUid);
-
-  try {
-    const userRecord = await db.collection('users').doc(uid).get();
-    // console.log('User record to be deleted:', userRecord);
-
-    if (userRecord.exists && userRecord.data().createdBy === adminUid) {
-      await admin.auth().deleteUser(uid);
-      await db.collection('users').doc(uid).delete();
-      // console.log('User deleted:', uid);
-      res.status(200).json({ message: 'User deleted successfully' });
-    } else {
-      // console.log('Permission denied: User not created by this admin');
-      res.status(403).json({ error: 'Permission denied' });
-    }
-  } catch (error) {
-    console.error('Error deleting user:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
 module.exports = {
   verifyAdmin,
-  createUser,
-  deleteUser,
 };
