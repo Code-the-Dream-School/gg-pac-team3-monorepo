325fc6b7e9d4761110b5afbe2f84097272188d20
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-08-04 09:52:43 -0400

    Update adminRoutes.js
    The team is on board with deleting create students functionality, so I deleted the function to create a new student, and the function to delete students.

--- gg-pac-team3-back/src/routes/adminRoutes.js
+++ gg-pac-team3-back/src/routes/adminRoutes.js
@@ -2,8 +2,4 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
-// Define the admin routes
-router.post('/create-user', adminController.verifyAdmin, adminController.createUser);
-router.delete('/delete-user/:uid', adminController.verifyAdmin, adminController.deleteUser);
-
 module.exports = router;
