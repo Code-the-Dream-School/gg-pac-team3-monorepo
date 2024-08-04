5d96e346c9fed50d1512a647f2996a483a76ccb9
SonaliBedge <146473259+SonaliBedge@users.noreply.github.com>
2024-08-04 09:20:07 -0700

    Update UsersModel.js
    Removed lastLogin and lastLogout fields and replaced admin with teacher in the database user collection.

--- gg-pac-team3-back/src/models/UsersModel.js
+++ gg-pac-team3-back/src/models/UsersModel.js
@@ -1,54 +1,46 @@
-const admin = require('../config/firebase'); // Import the Firebase Admin SDK
-
 class UsersModel {
-    constructor({
-      name,
-      email,
-      profilePicture = 'http://localhost:8000/images/userProfileImage.jpg', // Default image URL
-      loginType = null,
-      adminCode = null,
-      createdAt = new Date(),
-      lastLogin = null,
-      lastLogout = null
-    }) {
-      this.name = name;
-      this.email = email;
-      this.profilePicture = profilePicture;
-      this.loginType = loginType;
-      this.adminCode = adminCode;
-      this.createdAt = createdAt;
-      this.lastLogin = lastLogin;
-      this.lastLogout = lastLogout;
-    }
-  
-    static fromFirestore(doc) {
-      const data = doc.data();
-      return new UserModel({
-        name: data.name,
-        email: data.email,
-        profilePicture: data.profilePicture,
-        loginType: data.loginType,
-        adminCode: data.adminCode,
-        createdAt: data.createdAt.toDate(),
-        lastLogin: data.lastLogin ? data.lastLogin.toDate() : null,
-        lastLogout: data.lastLogout ? data.lastLogout.toDate() : null
-      });
-    }
-  
-    toFirestore() {
-      return {
-        name: this.name,
-        email: this.email,
-        profilePicture: this.profilePicture,
-        loginType: this.loginType,
-        adminCode: this.adminCode,
-      // Use admin.firestore.FieldValue for server timestamps
-        createdAt: admin.firestore.FieldValue.serverTimestamp(),
-        lastLogin: this.lastLogin ? admin.firestore.Timestamp.fromDate(this.lastLogin) : null,
-        lastLogout: this.lastLogout ? admin.firestore.Timestamp.fromDate(this.lastLogout) : null
-      };
-    }
+  constructor({
+    name,
+    email,
+    isTeacher,
+    profilePicture = 'http://localhost:8000/images/userProfileImage.jpg', // Default image URL
+    loginType = null,
+    teacherCode = null,
+    createdAt = new Date()      
+  }) {
+    this.name = name;
+    this.email = email;
+    this.isTeacher = isTeacher,
+    this.profilePicture = profilePicture;
+    this.loginType = loginType;
+    this.teacherCode = teacherCode;
+    this.createdAt = createdAt;     
   }
-  
-  module.exports = UsersModel;
-  
+
+  static fromFirestore(doc) {
+    const data = doc.data();
+    return new UserModel({
+      name: data.name,
+      email: data.email,
+      isTeacher: data.isTeacher,
+      profilePicture: data.profilePicture,
+      loginType: data.loginType,
+      teacherCode: data.teacherCode,
+      createdAt: data.createdAt.toDate(),        
+    });
+  }
+
+  toFirestore() {
+    return {
+      name: this.name,
+      email: this.email,
+      isTeacher: this.isTeacher,
+      profilePicture: this.profilePicture,
+      loginType: this.loginType,
+      teacherCode: this.teacherCode,
+      createdAt: admin.firestore.FieldValue.serverTimestamp()        
+    };
+  }
+}
+
+module.exports = UsersModel;
