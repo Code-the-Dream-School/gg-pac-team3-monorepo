d89f4fb468bf3a59c783996a8f09a3516c7ab66b
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-08-06 22:19:03 -0400

    Adding routes, file structure, and front page

---
+++ gg-pac-team3-front/src/assets/pages/frontpage.jsx
@@ -0,0 +1,12 @@
+import NavBarBlue from "../components/navBlue";
+import Panel from "../components/panel";
+
+function FrontPage(){
+    return(
+        <>
+            <NavBarBlue/>
+            <Panel/>
+        </>
+    )
+}
+export default FrontPage;
