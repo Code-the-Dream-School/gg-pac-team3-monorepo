f5a243a1af2148d8c99ea3382cae2f18c57d0d6d
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-08-08 13:10:54 -0400

    made changes to nav and file name when importing

--- gg-pac-team3-front/src/App.jsx
+++ gg-pac-team3-front/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import FrontPage from './pages/frontPage/frontpage';
-
+import FrontPage from './pages/frontPage/FrontPage';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={FrontPage()} />
+        <Route path="/" element={<FrontPage isLoggedIn={isLoggedIn} />} />
 
       </Routes>
     </BrowserRouter>
