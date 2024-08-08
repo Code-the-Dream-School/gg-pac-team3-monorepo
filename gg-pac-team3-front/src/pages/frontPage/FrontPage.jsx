1ba0143082e1123ab1249ff07df31eb862ae7930
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-08-07 21:40:20 -0400

    file structure updated

---
+++ gg-pac-team3-front/src/pages/frontPage/FrontPage.jsx
@@ -0,0 +1,12 @@
+import NavBlue from "../../components/layout/navBlue";
+import Panel from "./Panel";
+
+function FrontPage(){
+    return(
+        <>
+            <NavBlue/>
+            <Panel/>
+        </>
+    )
+}
+export default FrontPage;
