d9b83d9960b19d30b51e27a70740d44ecbdc10b9
Laura <laura.duranvelaz@gmail.com>
2024-08-23 18:07:09 -0400

    Complete front page (#16)
    In this merge:
    * Complete front page
    
    * Converted error and loading to components, switched to async/await, and now correctly handling errors
    
    * updated readme and minor styling changes

--- frontend/src/pages/frontPage/FrontPage.jsx
+++ frontend/src/pages/frontPage/FrontPage.jsx
@@ -1,16 +1,95 @@
-import styling from "./panel.module.css";
+import styling from "./frontpage.module.css";
 import panelImg from "../../assets/images/panelImage.jpg";
+import pencil from "../../assets/images/brokenPencil.png";
+import { useEffect } from "react";
+import { getCourses } from "../../services/api";
+import { useState } from "react";
+
 
 const FrontPage = () => {
+    const [courseDescriptions, setCoursesDescriptions] = useState([]);
+    const [state, setState] = useState({
+        loading: true,
+        error: false
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getCourses();
+                if(data.success === false){
+                    setState({
+                        loading: false,
+                        error: true
+                    })
+                }else{
+                    setCoursesDescriptions(data);
+                    setState({
+                        loading:false,
+                        error:false
+                    })
+                }
+            }catch(error){
+                setState({
+                    loading:false,
+                    error:true
+                })
+            }
+        }
+        fetchData()
+    },[])
+
+    if(state.error){
+        return <Error/>
+    }
+    else if(state.loading){
+        return <Loading/>
+    }
     return(
         <>
-            <div className={styling.container}>
-                <img id={styling.panel} src={panelImg}/>
-                <div className={styling.panelText}>
-                    <h3>Join LearningHub to manage or inspire your students effortlessly or sign up and start learning the fundamentals of coding </h3>
+        <div className={styling.panelContainer}>
+            <img className={styling.panel} src={panelImg}/>
+            <div className={styling.panelText}>
+                <h3>Welcome to LearningHub: Your go-to platform for managing and inspiring students or diving into a wide range of educational materials. Whether you're an educator looking to streamline your classroom or a learner eager to explore new topics, join us today to enhance your teaching experience or expand your knowledge across various subjects.</h3>
+            </div>
+        </div>
+        <div className={styling.mainContainer}>
+            <h3>Checkout our available courses:</h3>
+                <div className={styling.coursesContainer}>
+                    {courseDescriptions.map((course,index) => (
+                        <div className={styling.courseContainer} key={index}>
+                            <img className={styling.courseLogo} src={course.imageUrl} alt="Course Image"/>
+                            <div className={styling.courseDescription}>
+                                <h1>{course.courseName}</h1>
+                                <p>Description: {course.description}</p>
+                                <p>Rating: {course.rating}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            </div>     
+        </div>
+        </>
+    )        
+}
+export default FrontPage;
+
+
+const Error = () =>{
+    return(
+        <div className={styling.errorContainer}>
+            <img src={pencil} className={styling.pencilImg}/>
+            <h1>Oops, something went wrong!</h1>
+            <p>We've encountered an error while trying to display our current available courses. Please wait a moment and try again.</p>
+        </div>
+    )
+}
+
+const Loading = () => {
+    return(
+        <>
+        <h1>Loading...</h1>
+        <div className={styling.ldsspinner}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <p>Thank you for your patience.</p>
         </>
     )
 }
-export default FrontPage;
