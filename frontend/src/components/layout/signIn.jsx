0c603a8600787ea9d8f4aa1b0b84438496bbd86d
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2024-08-22 18:16:21 -0700

    Sign up (#8)
    * SignUpPageStyling
    
    Fiinished the Forms!
    
    * Fixed this

---
+++ frontend/src/components/layout/signIn.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./SignIn.module.css";
+
+const SignIn = ({ switchForm }) => {
+  return (
+    <div className={styles.container}>
+      <section className={styles.headings}>
+        <h1 className={styles.header}>Log In</h1>
+        <p className={styles.name}>Welcome Back to LearnHub</p>
+      </section>
+
+      <div className={styles.forms}>
+        <label className={styles.formName} htmlFor="email">
+          Email
+        </label>
+        <input className={styles.input} id="email" placeholder="Email"></input>
+
+        <label className={styles.formName} htmlFor="password">
+          Password
+        </label>
+        <input
+          className={styles.input}
+          id="password"
+          placeholder="Password"
+          type="password"
+        ></input>
+        <button className={styles.button}>Log In</button>
+      </div>
+
+      <section className={styles.closingSection}>
+        <p className={styles.content}>
+          Don’t have an account?{" "}
+          <a
+            className={styles.join}
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("Sign up now clicked"); // Add logging here
+              switchForm("Register"); // Switch to SignUp form
+            }}
+          >
+            Sign up now
+          </a>
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default SignIn;
