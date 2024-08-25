7ce4095110599916b17c72094e3f31502af1014a
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-08-24 22:04:46 -0400

    course and lesson test

--- server/models/LessonModel.mjs
+++ server/models/LessonModel.mjs
@@ -2,7 +2,6 @@ import admin from '../config/firebase.mjs';
 
 class LessonModel {
   constructor({
-    lessonId,
     title,
     description = {}, // Default to empty object if not provided
     points = 0,
@@ -11,7 +10,6 @@ class LessonModel {
     materials = ''
   }) {
     // Validate types
-    if (typeof lessonId !== 'string') throw new Error('Invalid lessonId');
     if (typeof title !== 'string') throw new Error('Invalid title');
     if (typeof points !== 'number') throw new Error('Invalid points');
     if (typeof order !== 'number') throw new Error('Invalid order');
@@ -19,7 +17,6 @@ class LessonModel {
     if (typeof materials !== 'string') throw new Error('Invalid materials');
     if (typeof description !== 'object') throw new Error('Invalid description');
 
-    this.lessonId = lessonId;
     this.title = title;
     this.description = description;
     this.points = points;
@@ -31,7 +28,6 @@ class LessonModel {
   // Convert to Firestore format
   toFirestore() {
     return {
-      lessonId: this.lessonId,
       title: this.title,
       description: this.description,
       points: this.points,
@@ -42,16 +38,16 @@ class LessonModel {
   }
 
   // Convert from Firestore format
-  static fromFirestore(data) {
-    return new LessonModel({
-      lessonId: data.lessonId,
+  static fromFirestore(data, lessonId) {
+    return {
+      lessonId: lessonId,  // Firestore document ID
       title: data.title,
       description: data.description || {}, // Ensure default to empty object if not present
       points: data.points || 0, // Ensure default to 0 if not present
       order: data.order || 0, // Ensure default to 0 if not present
       videoLinks: data.videoLinks || [], // Ensure default to empty array if not present
       materials: data.materials || '' // Ensure default to empty string if not present
-    });
+    };
   }
 }
 
