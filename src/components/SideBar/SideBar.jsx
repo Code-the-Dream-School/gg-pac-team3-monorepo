4ac37cb42bda0d66a60504c3c62155d3d18e23cf
SonaliBedge <bedgesonali@yahoo.com>
2024-08-30 14:33:38 -0700

    made changes

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,13 @@
-import SideBarItems from "../SideBarItems/SideBarItems.jsx";
+import SideBarItems from '../SideBarItems/SideBarItems.jsx';
 import styles from './SideBar.module.css';
 
-
 const SideBar = () => {
-
-    return (
-        <nav className={styles.sideBarContainer}>
-            <button className={styles.newCourseButton}>New Course</button>
-            <SideBarItems />
-        </nav>
-    );
+  return (
+    <nav className={styles.sideBarContainer}>
+      <button className={styles.newCourseButton}>New Course</button>
+      <SideBarItems />
+    </nav>
+  );
 };
 
 export default SideBar;
