565929739ebbc55f0e5044803237202234a10663
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-08-30 10:08:16 -0400

    updated based on comments

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,15 +1,8 @@
 export default {
-  transform: {
-    '^.+\\.mjs$': 'babel-jest',
-  },
+  transform: { },
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'jsx', 'mjs', 'ts', 'tsx', 'json', 'node'],
   transformIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/__tests__/**/*.mjs'],
   moduleDirectories: ['node_modules', '<rootDir>'], 
-  globals: {
-    'babel-jest': {
-      useESM: true,
-    },
-  },
 };
