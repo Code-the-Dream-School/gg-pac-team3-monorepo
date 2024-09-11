9c73314490800d2202dd8f9ec7ebf8187484478c
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-09-11 17:48:56 -0400

    routes corrected base on render log (#80)

--- server/routes/userFeedbackRoutes.mjs
+++ server/routes/userFeedbackRoutes.mjs
@@ -7,10 +7,10 @@ import { verifyToken } from '../middlewares/verifyToken.mjs';
 
 const router = express.Router();
 
-router.post('/UserFeedback/AddUserFeedback', verifyToken, AddUserFeedback);
+router.post('/AddUserFeedback', verifyToken, AddUserFeedback);
 
 router.get(
-  '/UserFeedback/:courseId',
+  '/:courseId',
   verifyToken,
   getAllUserFeedbackByCourseId
 );
