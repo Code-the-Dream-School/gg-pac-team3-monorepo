cbb7b36f4203fe09883b8179d5f21078d032237e
EdithHarrison <36203185+EdithHarrison@users.noreply.github.com>
2024-09-11 18:48:26 -0400

    remove log and static as they are not working anyway

--- server/server.mjs
+++ server/server.mjs
@@ -8,10 +8,4 @@ const { PORT = 8000 } = process.env;
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 app.listen(PORT, listener);
 
-// Log all registered routes to check for issues
-app._router.stack.forEach(function(r) {
-    if (r.route && r.route.path) {
-      console.log(`Registered route: ${r.route.path}`);
-    }
-  });
   
